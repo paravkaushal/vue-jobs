0d03b68ffe1f2fa438062e9580ab5b2ef534ef01
Parav Kaushal <parav.kaushal@renaissance.com>
2025-04-30 16:05:57 +0530

    fix: update API proxy target and remove Vue DevTools plugin

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,12 @@ export default defineConfig({
   base: '/',
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: "http://localhost:8000",
+        target: "https://json-server-xguj.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '') 
       }
